e53d74dcc0a13b33c34fcdfe836431630616ad76
Dmitriy Serdtsev <in19farkt@gmail.com>
2020-08-18 14:01:49 +0300

    Reward -> SReward

--- src/entities/createReward.ts
+++ src/entities/createReward.ts
@@ -1,4 +1,4 @@
-import { Reward } from "../../generated/schema";
+import { SReward } from "../../generated/schema";
 import { RewardDistribution } from "../../generated/SavingsModule/SavingsModule";
 import { getUniqId } from "../utils";
 import { Address } from "@graphprotocol/graph-ts";
@@ -7,7 +7,7 @@ export function createReward(
   event: RewardDistribution,
   poolAddress: Address
 ): void {
-  let reward = new Reward(getUniqId(event));
+  let reward = new SReward(getUniqId(event));
   reward.pool = poolAddress.toHex();
   reward.token = event.params.rewardRoken.toHex();
   reward.amount = event.params.amount;
