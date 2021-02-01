2bc9b6f51d2eaaf5a5df85643c4db9a3a6e999fd
Dmitriy Serdtsev <in19farkt@gmail.com>
2021-02-01 15:03:26 +0300

    Fix vaults V1 setWithdrawalFee handler

--- src/mappings/yVaultStrategy.ts
+++ src/mappings/yVaultStrategy.ts
@@ -1,8 +1,9 @@
 import { dataSource } from "@graphprotocol/graph-ts";
+import { SetWithdrawalFeeCall } from "../../generated/templates/YVaultStrategy/YVaultStrategy";
 
 import { updateStrategy } from "../entities";
 
-export function handleSetWithdrawalFee(): void {
+export function handleSetWithdrawalFee(call: SetWithdrawalFeeCall): void {
   let yVaultStrategyAddress = dataSource.address();
   updateStrategy(yVaultStrategyAddress);
 }
