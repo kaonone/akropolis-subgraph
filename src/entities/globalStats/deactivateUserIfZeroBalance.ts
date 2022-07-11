c0668ce8b68f84b3cce18a9c6d141d7ec3d5bbbf
imin314 <imin314@gmail.com>
2022-07-11 10:23:45 +0700

    Delete YearnVaultSavings integration

--- src/entities/globalStats/deactivateUserIfZeroBalance.ts
+++ src/entities/globalStats/deactivateUserIfZeroBalance.ts
@@ -4,7 +4,7 @@ import { User } from "../../../generated/schema";
 import { loadGlobalStat } from "./loadGlobalStats";
 
 export function deactivateUserIfZeroBalance(user: User): void {
-  let areAllUserPoolsEmpty = !user.stakingPools.length && !user.yearnVaults.length && !user.basisVaults.length;
+  let areAllUserPoolsEmpty = !user.stakingPools.length && !user.basisVaults.length;
   if (user.active && areAllUserPoolsEmpty) {
     let globalStat = loadGlobalStat();
     globalStat.activeMembersCount -= 1;
